27bc35e8f87bd561281bd9f3738b2f5bab949785
Duerst <duerst@demogr.mpg.de>
2020-11-26 16:11:23 +0100

    Update data warangling for exercise 5

--- data/create_human.R
+++ data/create_human.R
@@ -23,17 +23,19 @@ summary(gii)
 
 # rename variables with short and meaningful names
 # source: http://hdr.undp.org/en/content/human-development-index-hdi
-names(hd) <- c("gni_rank", "cntr", "hdi", "e0", "exp_edu", "mean_edu", "gni", "gni_minus_rank")
-names(gii) <- c("gii_rank", "cntr", "gii", "mmr", "abr", "parlia", "sec_edu_f", "sec_edu_m", "lfp_f", "lfp_m")
+names(hd) <- c("gni_rank", "cntr", "hdi", "e0", "exp_edu", "mean_edu", "gni",
+               "gni_minus_rank")
+names(gii) <- c("gii_rank", "cntr", "gii", "mmr", "abr", "parlia", "sec_edu_f",
+                "sec_edu_m", "lfp_f", "lfp_m")
 
 # create new variables for gender inequality data:
 # ratio of female and male population with secondary education for each country
 gii <- mutate(gii, sec_edu_sexratio = sec_edu_f / sec_edu_m)
-# ratioj of labour force participation of females and males for each country
+# ratio of labor force participation of females and males for each country
 gii <- mutate(gii, lfp_sexratio = lfp_f / lfp_m)
 
 
-# join the dats sets
+# join the data sets
 human <- inner_join(hd, gii, by = "cntr")
 dim(human) #correct number of observations and variables
 
@@ -57,11 +59,37 @@ human <- read.table("data\\human.csv", sep = ",")
 str(human)
 dim(human) # 195 observations and 19 variables, as it should be
 
+# The data used for this exercise comes from the United Nations Development
+# Programme (see <http://hdr.undp.org/en/content/human-development-index-hdi>)
+# and is a combination of two data sets containing information on the Human
+# Development Index (HDI) and the Gender Inequality Index (GII) for most
+# countries of the world (195 countries). The following list explains the
+# meaning of the most important of the 19 variables:
+# 
+# cntr: Country
+# hdi: Human Development Index
+# e0: Life expectancy at birth
+# exp_edu: Expected years of schooling
+# mean_edu: mean level of education
+# gni: Gross National Income 
+# gii: Gender Inequality Index
+# mmr: Maternal Mortality Ratio
+# abr: Adolescent birth rate
+# parlia: Percentage of female representatives in parliament
+# sec_edu_f: Proportion of females with at least secondary education
+# sec_edu_m: Proportion of males with at least secondary education
+# lfp_f: Proportion of females in the labor force
+# lfp_m: Proportion of males in the labor force
+# sec_edu_sexratio: Ratio of female and male population with secondary education
+# lfp_sexratio: Ratio of female and male labor force participation 
+
+
 # transform GNI variable to numeric
 human$gni <- str_replace(human$gni, pattern = ",", replace = "") %>% as.numeric()
 
 # exclude unneeded variables
-keep = c("cntr", "sec_edu_sexratio", "lfp_sexratio", "exp_edu", "e0", "gni", "mmr", "abr", "parlia")
+keep = c("cntr", "sec_edu_sexratio", "lfp_sexratio", "exp_edu", "e0", "gni",
+         "mmr", "abr", "parlia")
 human_new <- dplyr::select(human, one_of(keep))
 
 # exclude observations with missing values
@@ -70,12 +98,12 @@ human_new <- filter(human_new, complete.cases(human_new))
 # remove observations that relate to regions and not countries (the last 6 in the data set)
 my_human <- human_new[1:(nrow(human_new)-7),]
 
-# transform cntr variable to rownames
+# transform cntr variable to row names
 my_human <- my_human %>% remove_rownames() %>% column_to_rownames("cntr")
 
 # check dimensions
 dim(my_human) # everything as it should be
 
-# overrite old human data with new human data
+# overwrite old human data with new human data
 write.table(my_human, file = "data\\human.csv", sep = ",", row.names = TRUE)
 
